ff4662e0f4b5fa7564c338d2ee252e0c7437e1bd
Sandeep Kumar <132326008+sandeep5647@users.noreply.github.com>
2024-07-03 19:40:09 +0530

    url updated

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://live-chat-dblc.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
--- SocketIO/server.js
+++ SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://live-chat-dblc.onrender.com",
     methods: ["GET", "POST"],
   },
 });
